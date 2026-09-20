be9fc7e5f75d96a2ec90b81df999820dc6fa13ae
janson2026lu-sudo <janson2026lu@gmail.com>
2026-09-20 22:45:15 +0800

    Refresh offline cache for v5

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='rkzx-share-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='rkzx-v5';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./styles.css','./data-base.js','./q1-6.js','./q7-12.js','./q13-18.js','./past-exams.js','./app.js','./manifest.webmanifest'])))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
