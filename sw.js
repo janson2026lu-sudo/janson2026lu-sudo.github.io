3a6c9de7c83b2b3ef40166f15cd6b12cfad5fa6a
janson2026lu-sudo <janson2026lu@gmail.com>
2026-09-20 23:16:08 +0800

    Refresh cache for corrected case reference answers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rkzx-v10';
+const C='rkzx-v11';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./styles.css','./data-base.js','./q1-6.js','./q7-12.js','./q13-18.js','./past-exams.js','./past-exams-extra.js','./case-guides.js','./app.js','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
